4bd7da0f8d8b8021a757be6465e162a12f611bf6
Hamukaze <maplecow@gmail.com>
2022-07-15 16:52:24 +0900

    Added notes on game flow

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const gameBoard = (() => {
-    let gameState = [['x', 'x', 'x'], ['o', 'o', 'o'], ['x', 'x', 'x']];
+    let gameState = [['x', 'x', 'x'], 
+                     ['o', 'o', 'o'], 
+                     ['x', 'x', 'x']];
+    let currMarker = '';
     return{
         gameState,
+        currMarker,
     };
 })();
 
@@ -14,3 +18,26 @@ const Player = (name) => {
 const ticTacToe = (()=> {
     
 })
+
+const displayController = (()=> {
+    const renderBoard = () => {
+        const cellList = document.querySelectorAll(".cell");
+        
+    }
+})
+//Game flow
+//gameboard, player forms are rendered
+//Players key in names (once)
+//player forms are removed
+//Players are assigned markers
+//game proceeds until a win or a tie
+//game is reset
+
+//required functions
+//take gameState and render it onto gameboard div
+//onclick function to mark a cell with marker
+//function to tell which marker it currently is
+//function to initialize players from form
+//function to determie starting player
+//function to determine when someone wins or game ties
+//
